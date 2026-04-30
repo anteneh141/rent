1e6cdff7db9dbf13ed88cbd1f2a4bb64f5ffd743
unknown <you@example.com>
2026-04-30 17:26:54 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+/* function ant(name){
+    return console.log(`hi there i am ${name}`)
+}
+ant("anteneh")
+ const user ={
+    name: 'anteneh',
+    age:21,
+    greet(){
+        console.log(`who is the bas ass${}`)
+    }
+ } */
+const colors=['red','tan','blue']
+const color=colors.forEach(color=>console.log(color))
